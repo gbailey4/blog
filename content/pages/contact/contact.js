52eaef3660a47db0bce1f69931bcfccad12205e8
graham <gbailey4@gmail.com>
2020-07-01 19:05:31 -0700

    changed back to inline styles

--- content/pages/contact/contact.js
+++ content/pages/contact/contact.js
@@ -1,7 +1,20 @@
 import React from "react";
-import "./form.css";
 
 export default function Contact() {
+  const labelStyle = { fontSize: "1.25rem" };
+  const formInputStyle = {
+    width: "100%",
+    padding: "10px 15px",
+    border: "2px solid #696969",
+    borderRadius: "4px",
+  };
+  const textAreaStyle = {
+    height: "6em",
+  };
+  const buttonStyle = {
+    fontSize: "1.25rem",
+    fontWeight: "600",
+  };
   return (
     <form
       name="contact"
@@ -17,28 +30,46 @@ export default function Contact() {
         </label>
       </p>
       <p>
-        <label>
-          Name: <input type="text" name="name" className="formInput" required />
+        <label style={labelStyle}>
+          Name:{" "}
+          <input
+            type="text"
+            name="name"
+            className="formInput"
+            style={formInputStyle}
+            required
+          />
         </label>
       </p>
       <p>
-        <label>
+        <label style={labelStyle}>
           Email:{" "}
-          <input type="email" name="email" className="formInput" required />
+          <input
+            type="email"
+            name="email"
+            className="formInput"
+            style={formInputStyle}
+            required
+          />
         </label>
       </p>
       <p>
-        <label>
+        <label style={labelStyle}>
           Message:{" "}
           <textarea
             name="message"
             className="formInput textareaFormInput"
+            style={{ ...formInputStyle, ...textAreaStyle }}
             required
           ></textarea>
         </label>
       </p>
       <p>
-        <button type="submit" className="formInput">
+        <button
+          type="submit"
+          className="formInput"
+          style={{ ...formInputStyle, ...buttonStyle }}
+        >
           Get in Touch
         </button>
       </p>
